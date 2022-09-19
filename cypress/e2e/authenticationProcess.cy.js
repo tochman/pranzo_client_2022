8b91dd2e926cb71b55f6c1098343eac765638fdd
tochman <thomas@craftacademy.se>
2022-09-19 15:32:02 +0200

    WIP logout process updates icon for color mode)

--- cypress/e2e/authenticationProcess.cy.js
+++ cypress/e2e/authenticationProcess.cy.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f0b45731788b524072ff0fbab8612726d93ef203d6e1503c5d3b4b1afa952bd
-size 3056
+oid sha256:9c1fefde7bac3cb5fc0b9ab65d56577e3d5c4163de8183e4d7f55865074af0fb
+size 3061
