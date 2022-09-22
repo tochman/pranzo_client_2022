1616b894e849d3436ca491441dac01a807cd34f3
tochman <thomas@craftacademy.se>
2022-09-22 13:47:39 +0200

    refactors logo in header and footer

--- src/components/content/Logo.jsx
+++ src/components/content/Logo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc96aacab8f7d0cc818365c0d07f6afa1ea932961bad6f079888ee0c181a7b76
-size 427
+oid sha256:21149856be8be6296de32cd9355cec3386e05d3f97a6dbfe16bd8ff1f998d72e
+size 465
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:923631791c871ae86a6fca5deb1743d15cb9b7a6419a4496d9d7d5edca9f1667
-size 1857
+oid sha256:64ec726341641093c070878adb8209dd98142a88a98b9236a67f10ee70c91afd
+size 1847
