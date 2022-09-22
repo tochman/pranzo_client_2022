4fc97e4d6c21040b3ee4ea435a3739fec997d34c
tochman <thomas@craftacademy.se>
2022-09-22 13:47:39 +0200

    refactors logo in header and footer

--- src/components/content/Logo.jsx
+++ src/components/content/Logo.jsx
@@ -2,13 +2,14 @@ import { Image, useColorModeValue } from "@chakra-ui/react";
 import colorLogo from "../../assets/pranzo_color.png";
 import whiteLogo from "../../assets/pranzo_white.png";
 
-const Logo = ({width, height}) => {
+const Logo = ({ width, height }) => {
   return (
     <Image
-      htmlWidth={width ? width : "120px"}
+      htmlWidth={width ? width : "110px"}
       htmlHeight={height ? height : "auto"}
       objectFit="fit"
       src={useColorModeValue(colorLogo, whiteLogo)}
+      style={{ cursor: "pointer" }}
     />
   );
 };
