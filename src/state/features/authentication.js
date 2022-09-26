f6c2c712988f878d2d5f65bded40a3263d8de8e3
tochman <thomas@craftacademy.se>
2022-09-26 11:55:33 +0200

    HOTFIX missing call to fetch vouchers on signIn removes thet call on register :zap:

--- src/state/features/authentication.js
+++ src/state/features/authentication.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:111f7a29be952c90454abad9f394717acffdd4db0d1f848098c1055bec43d967
-size 2987
+oid sha256:40e8ac2e90accc3663a3955dd177f7e67b3e9470d96edc85836e392b953386d3
+size 2860
