73558c76a12a71aa389b2fc3be7efbf926ff798c
tochman <thomas@craftacademy.se>
2022-09-26 10:15:04 +0200

    adds specs for other voucher statuses and variants

--- cypress/e2e/voucherHandling.cy.js
+++ cypress/e2e/voucherHandling.cy.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e1ef89305076af42da022f6bde4c39828cdc03d81a4845e3e8ff70b71b08564
-size 1340
+oid sha256:6b3f138b6523623180e16d32a0522027f82e43c145accd08cc1ccd1f728c3e01
+size 2481
--- src/components/dashboard/Vouchers.jsx
+++ src/components/dashboard/Vouchers.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e3401ec7269545944be00cedbb3254b605c578175a8f11036ac7dbc493ed621
-size 5116
+oid sha256:172a890ba8a4eab23f1399c18009ac49a5726ffe9cca0c3699c4cb3a6feb0788
+size 5191
