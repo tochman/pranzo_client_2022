25fd00b66a447b2edf463a0f3ec9e8e23062d589
tochman <thomas@craftacademy.se>
2022-09-26 08:17:06 +0200

    adds a madal to display actions on voucher

--- src/components/dashboard/Vouchers.jsx
+++ src/components/dashboard/Vouchers.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64c722f0e45b124f3e2c792294c226dd44f3631bd39ac30e2d579f9462cd553e
-size 3724
+oid sha256:1e6251a3af6db99c949c2210cca633232197081fc1efcb64bb74c33a9677d190
+size 4250
