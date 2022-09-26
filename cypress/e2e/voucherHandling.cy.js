a76f51135c6200f89c9fc5216f36a99e453c4917
tochman <thomas@craftacademy.se>
2022-09-26 11:33:00 +0200

    removes only restriction on spec

--- cypress/e2e/voucherHandling.cy.js
+++ cypress/e2e/voucherHandling.cy.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d13a636692a68b0a73a76d81f944b4ed870ac512f9ef47f815af9d8d5b17a57
-size 2487
+oid sha256:24d753ab9099c3f0d4e65357548a8276e08a445e7a6660dfff5b19d023938eed
+size 2482
