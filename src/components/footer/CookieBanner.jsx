56885c739d79a2fa2e151a3ba36e7db2289d6ab6
tochman <thomas@craftacademy.se>
2022-12-28 18:23:31 +0100

    adds Josefin Sans font

--- src/components/footer/CookieBanner.jsx
+++ src/components/footer/CookieBanner.jsx
@@ -10,23 +10,27 @@ import { useCookieConsentContext } from "@use-cookie-consent/react";
 import Content from "../elements/Content";
 
 const CookieBanner = () => {
+  const { t } = useTranslation();
   const { acceptAllCookies, declineAllCookies, acceptCookies } =
     useCookieConsentContext();
 
   return (
-    <Alert data-cy='cookie-banner'>
+    <Alert data-cy="cookie-banner">
       <AlertIcon />
       <Stack spacing={2} direction={{ base: "column", md: "row" }}>
         <Box>
           <Heading size={"md"}>Vi bryr oss om din integritet</Heading>
           <Content size="sm">
-            Genom att klicka på "Acceptera alla" samtycker du till
-            lagring av cookies på din enhet för att förbättra navigeringen på
-            webbplatsen, analysera webbplatsens användning och bistå i våra
+            Genom att klicka på "Acceptera alla" samtycker du till lagring av
+            cookies på din enhet för att förbättra navigeringen på webbplatsen,
+            analysera webbplatsens användning och bistå i våra
             marknadsföringsinsatser.{" "}
           </Content>
         </Box>
-        <Stack direction={{ base: "row", md: "column" }} spacing={{base: 1, sm: 4, md: 1}}>
+        <Stack
+          direction={{ base: "row", md: "column" }}
+          spacing={{ base: 1, sm: 4, md: 1 }}
+        >
           <Button
             size={"sm"}
             colorScheme="orange"
