8c6b8b3256979d7b8afed888e77be948f5779f0a
tochman <thomas@craftacademy.se>
2023-01-13 23:26:10 +0100

    cleans up code in notifications

--- src/state/features/notifications.js
+++ src/state/features/notifications.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const slackApi = axios.create()
+const slackApi = axios.create();
 
 export const notifySlack = createAsyncThunk(
   "users/notifySlack",
@@ -15,9 +15,9 @@ export const notifySlack = createAsyncThunk(
           text: {
             type: "mrkdwn",
             text: `${data.data.name} just ${data.action} with Pranzo.se`,
-          }
-        }
-      ]
+          },
+        },
+      ],
     };
     const token = import.meta.env.VITE_SLACK_TOKEN;
     await slackApi.post(`https://hooks.slack.com/services/${token}`, payload, {
@@ -25,53 +25,5 @@ export const notifySlack = createAsyncThunk(
         "Content-type": "application/x-www-form-urlencoded",
       },
     });
-    // debugger
-    // let payload
-    // if (data.formSubmission) {
-    //   const text = `Has submitted an interest form for ${data.toSentence()}.\nUse ${
-    //     data.contact.email
-    //   } to get in touch.`;
-    //   payload = {
-    //     icon_emoji: ":pencil2:",
-    //     message: data.contact.message,
-    //     username: data.contact.name,
-    //     blocks: [
-    //       {
-    //         type: "section",
-    //         text: {
-    //           type: "mrkdwn",
-    //           text: `${text}\n\nMessage: ${data.contact.message}`,
-    //         },
-    //       },
-    //     ],
-    //   };
-    // } else {
-    //   payload = {
-    //     icon_emoji: ":rocket:",
-    //     username: "Notification Bot",
-    //     text: `There is a new ${data.actionType} from ${data.user.email}`,
-    //   };
-    // }
-    // console.warn("ENV PROD?: "+ import.meta.env.PROD)
-    // if (import.meta.env.PROD) {
-    //   // production code
-    //   const token = import.meta.env.VITE_SLACK_TOKEN
-    //   const resp = await axios.post(
-    //     `https://hooks.slack.com/services/${
-    //       token
-    //     }`,
-    //     payload,
-    //     {
-    //       headers: {
-    //         "Content-Type": "application/x-www-form-urlencoded",
-    //       },
-    //     }
-    //   );
-    //   return true;
-    // } else {
-    //   // dev code
-    //   debugger
-    //   return true;
-    // }
   }
 );
