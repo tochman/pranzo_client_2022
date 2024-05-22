047d491f3b3176388d5f9f4359507afd217e167c
tochman <thomas@craftacademy.se>
2024-05-22 07:16:27 +0200

    adds missig helmet to venue setup

--- src/components/dashboard/VenueSetup.jsx
+++ src/components/dashboard/VenueSetup.jsx
@@ -25,6 +25,7 @@ import { auth } from "../../state/utilities/authConfig";
 import { FiImage } from "react-icons/fi";
 import { toBase64 } from "../../modules/ImageEncoder";
 import { useRef, useState } from "react";
+import { Helmet } from "react-helmet";
 
 const VenueSetup = () => {
   const dispatch = useDispatch();
@@ -92,153 +93,157 @@ const VenueSetup = () => {
   };
 
   return (
-    <Stack minH={"80vh"} direction={{ base: "column", md: "row" }} m={1}>
-      <Flex p={8} flex={1} align={"top"} justify={"left"}>
-        <Stack spacing={4} w={"full"} maxW={"md"}>
-          <Heading fontSize={"2xl"}>
-            {edit || vendor
-              ? t("venue.edit.heading")
-              : t("venue.setup.heading")}
-          </Heading>
-          <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <FormControl isInvalid={errors.name}>
-              <FormLabel htmlFor="name">
-                {t("venue.formElements.venueName")}
-              </FormLabel>
-              <Input
-                defaultValue={(edit || vendor) && vendor.name}
-                data-cy="name"
-                id="name"
-                {...register("name", {
-                  required: t("forms.messages.required"),
-                  minLength: {
-                    value: 4,
-                    message: t("forms.messages.minLength", { length: 4 }),
-                  },
-                })}
-              />
-              <FormErrorMessage>
-                {errors.name && errors.name.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.vat_id}>
-              <FormLabel htmlFor="vat_id">
-                {t("venue.formElements.venueVatid")}
-              </FormLabel>
-              <Input
-                defaultValue={(edit || vendor) && vendor.vat_id}
-                data-cy="vat_id"
-                id="vat_id"
-                {...register("vat_id", {
-                  required: t("forms.messages.required"),
-                  pattern: {
-                    value: /^[a-zA-Z]{2}[0-9]{12}$/,
-                    message: t("forms.messages.invalidVat"),
-                  },
-                  minLength: {
-                    value: 4,
-                    message: t("forms.messages.minLength", { length: 4 }),
-                  },
-                })}
-              />
-              <FormErrorMessage>
-                {errors.vat_id && errors.vat_id.message}
-              </FormErrorMessage>
-              <FormHelperText>
-                {t("venue.formElements.venueVatidHelper")}
-              </FormHelperText>
-            </FormControl>
-            <FormControl isInvalid={errors.description}>
-              <FormLabel htmlFor="description">
-                {t("venue.formElements.description")}{" "}
-                {t("forms.elements.optional")}
-              </FormLabel>
-              <Textarea
-                data-cy="description"
-                defaultValue={(edit || vendor) && vendor.description}
-                id="description"
-                {...register("description")}
-              />
-              <FormErrorMessage>
-                {errors.description && errors.description.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={errors.primaryEmail}>
-              <FormLabel htmlFor="primaryEmail">
-                {t("venue.formElements.primaryEmail")}
-              </FormLabel>
-              <Input
-                data-cy="email"
-                defaultValue={(edit || vendor) && vendor.primary_email}
-                id="primaryEmail"
-                {...register("primaryEmail", {
-                  pattern: {
-                    value: emailRegex,
-                    message: t("forms.messages.invalidEmail"),
-                  },
-                  required: t("forms.messages.required"),
-                })}
-                onBlur={(event) => checkEmail(event.target.value)}
-              />
-              <FormErrorMessage>
-                {errors.primaryEmail && errors.primaryEmail.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl isInvalid={!edit && errors.logotype}>
-              <FormLabel>{t("forms.elements.logotype")}</FormLabel>
-              <InputGroup ref={inputRef}>
-                <InputLeftElement pointerEvents="none">
-                  <Icon as={FiImage} />
-                </InputLeftElement>
-                <input
-                  data-cy="logotype"
-                  type="file"
-                  name="logotype"
-                  accept={"image/*"}
-                  onInput={changedFile}
-                  style={{ display: "none" }}
-                  {...register("logotype", {
-                    required: !edit && t("forms.messages.required"),
+    <>
+      <Helmet>
+        {edit || vendor ? t("venue.edit.heading") : t("venue.setup.heading")}
+      </Helmet>
+      <Stack minH={"80vh"} direction={{ base: "column", md: "row" }} m={1}>
+        <Flex p={8} flex={1} align={"top"} justify={"left"}>
+          <Stack spacing={4} w={"full"} maxW={"md"}>
+            <Heading fontSize={"2xl"}>
+              {edit || vendor
+                ? t("venue.edit.heading")
+                : t("venue.setup.heading")}
+            </Heading>
+            <form onSubmit={handleSubmit(handleFormSubmit)}>
+              <FormControl isInvalid={errors.name}>
+                <FormLabel htmlFor="name">
+                  {t("venue.formElements.venueName")}
+                </FormLabel>
+                <Input
+                  defaultValue={(edit || vendor) && vendor.name}
+                  data-cy="name"
+                  id="name"
+                  {...register("name", {
+                    required: t("forms.messages.required"),
+                    minLength: {
+                      value: 4,
+                      message: t("forms.messages.minLength", { length: 4 }),
+                    },
                   })}
                 />
-                <Input
-                  data-cy="logotypeFake"
-                  placeholder={t("forms.elements.logotypePlaceholder")}
-                  onClick={() => inputRef.current.children.logotype.click()}
-                  readOnly={true}
-                  value={file && file.name}
-                />
-              </InputGroup>
-              {!edit && (
                 <FormErrorMessage>
-                  {errors.logotype && errors.logotype.message}
+                  {errors.name && errors.name.message}
                 </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.vat_id}>
+                <FormLabel htmlFor="vat_id">
+                  {t("venue.formElements.venueVatid")}
+                </FormLabel>
+                <Input
+                  defaultValue={(edit || vendor) && vendor.vat_id}
+                  data-cy="vat_id"
+                  id="vat_id"
+                  {...register("vat_id", {
+                    required: t("forms.messages.required"),
+                    pattern: {
+                      value: /^[a-zA-Z]{2}[0-9]{12}$/,
+                      message: t("forms.messages.invalidVat"),
+                    },
+                    minLength: {
+                      value: 4,
+                      message: t("forms.messages.minLength", { length: 4 }),
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.vat_id && errors.vat_id.message}
+                </FormErrorMessage>
+                <FormHelperText>
+                  {t("venue.formElements.venueVatidHelper")}
+                </FormHelperText>
+              </FormControl>
+              <FormControl isInvalid={errors.description}>
+                <FormLabel htmlFor="description">
+                  {t("venue.formElements.description")}{" "}
+                  {t("forms.elements.optional")}
+                </FormLabel>
+                <Textarea
+                  data-cy="description"
+                  defaultValue={(edit || vendor) && vendor.description}
+                  id="description"
+                  {...register("description")}
+                />
+                <FormErrorMessage>
+                  {errors.description && errors.description.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.primaryEmail}>
+                <FormLabel htmlFor="primaryEmail">
+                  {t("venue.formElements.primaryEmail")}
+                </FormLabel>
+                <Input
+                  data-cy="email"
+                  defaultValue={(edit || vendor) && vendor.primary_email}
+                  id="primaryEmail"
+                  {...register("primaryEmail", {
+                    pattern: {
+                      value: emailRegex,
+                      message: t("forms.messages.invalidEmail"),
+                    },
+                    required: t("forms.messages.required"),
+                  })}
+                  onBlur={(event) => checkEmail(event.target.value)}
+                />
+                <FormErrorMessage>
+                  {errors.primaryEmail && errors.primaryEmail.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!edit && errors.logotype}>
+                <FormLabel>{t("forms.elements.logotype")}</FormLabel>
+                <InputGroup ref={inputRef}>
+                  <InputLeftElement pointerEvents="none">
+                    <Icon as={FiImage} />
+                  </InputLeftElement>
+                  <input
+                    data-cy="logotype"
+                    type="file"
+                    name="logotype"
+                    accept={"image/*"}
+                    onInput={changedFile}
+                    style={{ display: "none" }}
+                    {...register("logotype", {
+                      required: !edit && t("forms.messages.required"),
+                    })}
+                  />
+                  <Input
+                    data-cy="logotypeFake"
+                    placeholder={t("forms.elements.logotypePlaceholder")}
+                    onClick={() => inputRef.current.children.logotype.click()}
+                    readOnly={true}
+                    value={file && file.name}
+                  />
+                </InputGroup>
+                {!edit && (
+                  <FormErrorMessage>
+                    {errors.logotype && errors.logotype.message}
+                  </FormErrorMessage>
+                )}
+              </FormControl>
+              {file && (
+                <Image
+                  src={file.content}
+                  width={"200px"}
+                  height={"auto"}
+                  paddingTop={5}
+                />
               )}
-            </FormControl>
-            {file && (
-              <Image
-                src={file.content}
-                width={"200px"}
-                height={"auto"}
-                paddingTop={5}
-              />
-            )}
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={isSubmitting}
-              type="submit"
-              data-cy="submit"
-              disabled={primaryEmailState.error}
-            >
-              {t("forms.elements.submit")}
-            </Button>
-          </form>
-        </Stack>
-      </Flex>
-    </Stack>
+              <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                type="submit"
+                data-cy="submit"
+                disabled={primaryEmailState.error}
+              >
+                {t("forms.elements.submit")}
+              </Button>
+            </form>
+          </Stack>
+        </Flex>
+      </Stack>
+    </>
   );
 };
 
 export default VenueSetup;
-
